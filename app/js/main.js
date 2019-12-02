73487fa4be710da824122f08ddf6bae0828a30cc
Shulhan Andrei <shulhan.andrei@gmail.com>
2019-12-02 12:47:32 +0300

    add tabs and slick.js

--- app/js/main.js
+++ app/js/main.js
@@ -2,4 +2,19 @@ $(function() {
     $('.navbar__menu').on('click', function() {
         $('.menu-icon').toggleClass('menu-icon-active');
     })
+
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 3,
+        arrows:true,
+        nextArrow: '<img class="prev" src="../img/icons/arrow-prev.svg">',
+        prevArrow: '<img class="next" src="../img/icons/arrow-next.svg">'
+      });
+
+      $('ul.portfolio-tabs__caption').on('click', 'li:not(.active)', function() {
+        $(this)
+          .addClass('active').siblings().removeClass('active')
+          .closest('div.portfolio-tabs').find('div.portfolio-tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+      });
 })
