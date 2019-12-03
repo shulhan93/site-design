ae92fd5c4d3021878ddbbbdf1c86f82d3d8e4d1f
Shulhan Andrei <shulhan.andrei@gmail.com>
2019-12-03 13:22:13 +0300

    add section services

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,8 @@ $(function() {
         $('.menu-icon').toggleClass('menu-icon-active');
     })
 
-    $('.slider').slick({
+    function slider(name) {
+      $(name).slick({
         infinite: true,
         slidesToShow: 4,
         slidesToScroll: 3,
@@ -11,10 +12,16 @@ $(function() {
         nextArrow: '<img class="prev" src="../img/icons/arrow-prev.svg">',
         prevArrow: '<img class="next" src="../img/icons/arrow-next.svg">'
       });
+    }
+    slider('.slider')
+    slider('.slider2')
+    slider('.slider3')
+
 
       $('ul.portfolio-tabs__caption').on('click', 'li:not(.active)', function() {
         $(this)
           .addClass('active').siblings().removeClass('active')
           .closest('div.portfolio-tabs').find('div.portfolio-tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+
       });
 })
