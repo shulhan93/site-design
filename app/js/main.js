6d3e4830c7c162b2e944b5dae80cf37f3a987a1f
Shulhan Andrei <shulhan.andrei@gmail.com>
2019-12-05 10:57:00 +0300

    add section services and video

--- app/js/main.js
+++ app/js/main.js
@@ -3,25 +3,43 @@ $(function() {
         $('.menu-icon').toggleClass('menu-icon-active');
     })
 
-    function slider(name) {
-      $(name).slick({
-        infinite: true,
-        slidesToShow: 4,
-        slidesToScroll: 3,
-        arrows:true,
-        nextArrow: '<img class="prev" src="../img/icons/arrow-prev.svg">',
-        prevArrow: '<img class="next" src="../img/icons/arrow-next.svg">'
+
+
+
+      function tabs(name) {
+        $('ul.' + name +'__caption').on('click', 'li:not(.active)', function() {
+          $(this)
+            .addClass('active').siblings().removeClass('active')
+            .closest('div.' + name).find('div.' + name +'__content').removeClass('active').eq($(this).index()).addClass('active animated slow fadeIn');
+        });
+      }
+  
+      tabs('tabs');
+      tabs('tabs2')
+
+      function slider(name) {
+        $(name).slick({
+          infinite: true,
+          slidesToShow: 4,
+          slidesToScroll: 3,
+          arrows:true,
+          nextArrow: '<img class="prev" src="../img/icons/arrow-prev.svg">',
+          prevArrow: '<img class="next" src="../img/icons/arrow-next.svg">'
+        });
+      }
+      slider('slider')
+      slider('slider2')
+      slider('slider3')
+
+      $('.popup-youtube').magnificPopup({
+        disableOn: 900,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+    
+        fixedContentPos: false
       });
-    }
-    slider('.slider')
-    slider('.slider2')
-    slider('.slider3')
 
 
-      $('ul.portfolio-tabs__caption').on('click', 'li:not(.active)', function() {
-        $(this)
-          .addClass('active').siblings().removeClass('active')
-          .closest('div.portfolio-tabs').find('div.portfolio-tabs__content').removeClass('active').eq($(this).index()).addClass('active');
-
-      });
 })
