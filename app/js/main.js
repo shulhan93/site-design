58bf555bd31844d06ae844514e8cac4500206f58
Shulhan Andrei <shulhan.andrei@gmail.com>
2019-12-15 16:38:40 +0300

    add slider for section reviews

--- app/js/main.js
+++ app/js/main.js
@@ -3,11 +3,10 @@ $(function() {
         $('.menu-icon').toggleClass('menu-icon-active');
     })
 
-
-
-
       function tabs(name) {
         $('ul.' + name +'__caption').on('click', 'li:not(.active)', function() {
+          console.log($(this))
+
           $(this)
             .addClass('active').siblings().removeClass('active')
             .closest('div.' + name).find('div.' + name +'__content').removeClass('active').eq($(this).index()).addClass('active animated slow fadeIn');
@@ -18,18 +17,33 @@ $(function() {
       tabs('tabs2')
 
       function slider(name) {
-        $(name).slick({
-          infinite: true,
-          slidesToShow: 4,
-          slidesToScroll: 3,
-          arrows:true,
-          nextArrow: '<img class="prev" src="../img/icons/arrow-prev.svg">',
-          prevArrow: '<img class="next" src="../img/icons/arrow-next.svg">'
-        });
+        if (name == '.slider4') {
+          $(name).slick({
+            infinite: true,
+            speed: 300,
+            slidesToShow: 3,
+            centerMode: true,
+            centerPadding: '200px',
+            variableWidth: true,
+            arrows: false,
+            dots: true,
+          });
+        } else {
+          $(name).slick({
+            infinite: true,
+            slidesToShow: 4,
+            slidesToScroll: 3,
+            arrows:true,
+            nextArrow: '<img class="prev" src="../img/icons/arrow-prev.svg">',
+            prevArrow: '<img class="next" src="../img/icons/arrow-next.svg">'
+          });
+        }
+    
       }
-      slider('slider')
-      slider('slider2')
-      slider('slider3')
+      slider('.slider')
+      slider('.slider2')
+      slider('.slider3')
+      slider('.slider4')
 
       $('.popup-youtube').magnificPopup({
         disableOn: 900,
